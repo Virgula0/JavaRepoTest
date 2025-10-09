2b982ac059157dcfc5c00bbc453d44c6839aefc7
Virgula0 <angelo.rosa@protonmail.com>
2025-10-09 16:48:17 +0200

    setup maven action

--- com.example.progetto.angelo.rosa.test/src/test/java/com/example/progetto/angelo/rosa/test/AppTest.java
+++ com.example.progetto.angelo.rosa.test/src/test/java/com/example/progetto/angelo/rosa/test/AppTest.java
@@ -1,38 +1,23 @@
 package com.example.progetto.angelo.rosa.test;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+	private App app;
+	
+	@Before
+	public void setup() {
+		app = new App();
+	}
+	
+	@Test
+	public void testSayHello() {
+		assertEquals("Hello", app.sayHello());
+	}
 }
