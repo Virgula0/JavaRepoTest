35e419c0c6258efc6dfbf89d7bd1d6739188f054
Virgula <ngelo.rosa@protonmail.com>
2025-10-09 16:04:37 +0200

    setupmaven-archetype-quickstart and change junit version in pol.xml

---
+++ com.example.progetto.angelo.rosa.test/src/main/java/com/example/progetto/angelo/rosa/test/App.java
@@ -0,0 +1,13 @@
+package com.example.progetto.angelo.rosa.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
