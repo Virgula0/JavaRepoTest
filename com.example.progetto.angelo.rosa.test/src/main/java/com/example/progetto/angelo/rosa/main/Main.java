a148129b5bdbc5943aef9af56034893fdd59858e
Angelo Rosa <angelo.rosa@protonmail.com>
2025-10-17 17:10:11 +0200

    setup (#5)

--- com.example.progetto.angelo.rosa.test/src/main/java/com/example/progetto/angelo/rosa/main/Main.java
+++ com.example.progetto.angelo.rosa.test/src/main/java/com/example/progetto/angelo/rosa/main/Main.java
@@ -1,15 +1,27 @@
 package com.example.progetto.angelo.rosa.main;
 
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+/**
+ * Simple app accessing MongoDB.
+ */
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		boolean a = false;
-		if (!a) {
-			System.out.println(false);
-		}
-		
-		System.out.println("Hello World");
+		String mongoHost = "mongodb"; // default hostname from Docker setup
+		if (args.length > 0)
+			mongoHost = args[0];
+		// default port for MongoDB is 27017
+		MongoClient mongoClient = new MongoClient(mongoHost);
+		MongoDatabase db = mongoClient.getDatabase("mydb");
+		MongoCollection<Document> collection = db.getCollection("examples");
+		Document doc = new Document("name", "Greeting").append("type", "Hello World!");
+		collection.insertOne(doc);
+		// should print "Hello World!"
+		System.out.println(collection.find().first().get("type"));
+		mongoClient.close();
 	}
-
 }
