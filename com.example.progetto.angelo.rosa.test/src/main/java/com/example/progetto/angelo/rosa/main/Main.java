b6a0f501dbd3b9ac0cc03b3575025867c5a85880
Angelo Rosa <angelo.rosa@protonmail.com>
2025-10-17 14:30:05 +0200

    add jacoco package exclusion logic in pom.xml (#4)
    * add jacoco package exclusion logic in pom.xml
    
    * add exclusions for pit too
    
    * format

---
+++ com.example.progetto.angelo.rosa.test/src/main/java/com/example/progetto/angelo/rosa/main/Main.java
@@ -0,0 +1,15 @@
+package com.example.progetto.angelo.rosa.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean a = false;
+		if (!a) {
+			System.out.println(false);
+		}
+		
+		System.out.println("Hello World");
+	}
+
+}
